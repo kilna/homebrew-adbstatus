0e5af5ba067e57c917c25ba772ff3c1beab249d4
Kilna <kilna@kilna.com>
2025-03-18 21:15:07 -0700

    Initial dev

--- Formula/adbstatus.rb
+++ Formula/adbstatus.rb
@@ -8,24 +8,20 @@ class Adbstatus < Formula
   
   depends_on "python@3"
   depends_on "sleepwatcher"
-  
-  resource "psutil" do
-    url "https://files.pythonhosted.org/packages/b2/38/c4d87f8a0ad6067a65ba112f273560ae721f068631d6ca546b059fc14cf4/psutil-5.9.8.tar.gz"
-    sha256 "6be126e3225486dff286a8fb9a06246a5253f4c7c53b475ea5f5ac934e64194c"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
-  end
-
-  resource "tomli" do
-    url "https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz"
-    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
-  end
 
   def install
-    virtualenv_install_with_resources
+    # Create a virtual environment
+    venv = libexec
+    virtualenv_create(venv, "python3")
+    
+    # Install dependencies directly with pip without specifying URLs
+    system libexec/"bin/pip", "install", "psutil", "pyyaml", "tomli"
+    
+    # Install the package itself (from the current directory)
+    system libexec/"bin/pip", "install", "-e", "."
+    
+    # Create bin stubs
+    bin.install_symlink Dir["#{libexec}/bin/adbstatus*"]
     
     # Create configuration directories
     (etc/"adbstatus/ssl").mkpath
