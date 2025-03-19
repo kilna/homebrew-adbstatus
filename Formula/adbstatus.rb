3fc4ed18df504fb92d40031184032c8d3acabce7
Kilna <kilna@kilna.com>
2025-03-18 21:06:51 -0700

    Initial dev

--- Formula/adbstatus.rb
+++ Formula/adbstatus.rb
@@ -1,4 +1,6 @@
 class Adbstatus < Formula
+  include Language::Python::Virtualenv
+  
   desc "Android Debug Bridge (ADB) device monitor with sleep/wake support"
   homepage "https://github.com/kilna/adbstatus"
   head "https://github.com/kilna/adbstatus.git", branch: "main"
@@ -8,44 +10,22 @@ class Adbstatus < Formula
   depends_on "sleepwatcher"
   
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/fe/8c/284d8d946e21c37bf0a7b59facf871470e35d7a468abbfbc31ef6d42099f/psutil-5.9.8.tar.gz"
+    url "https://files.pythonhosted.org/packages/d3/d3/fdfbdd26d1e84e15a2a019000f9d58d80485af2a3ddff4492b59c1f4cc7a/psutil-5.9.8.tar.gz"
     sha256 "6be126e3225486dff286a8fb9a06246a5253f4c7c53b475ea5f5ac934e64194c"
   end
 
-  resource "pyyaml" do
-    url "https://files.pythonhosted.org/packages/cd/e5/af35f7ea75cf72f2cd079c95ee16797de7cd71f29ea7c68ae5ce7be1eda0/PyYAML-6.0.1.tar.gz"
+  resource "PyYAML" do
+    url "https://files.pythonhosted.org/packages/db/04/4971e49f663b41aca3e6dab36e60a79df5bd57ced61d1ff26a2acb176b97/PyYAML-6.0.1.tar.gz"
     sha256 "bfdf460b1736c775f2ba9f6a92bca30bc2095067b8a9d77876d1fad6cc3b4a43"
   end
 
   resource "tomli" do
-    url "https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz"
-    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
+    url "https://files.pythonhosted.org/packages/97/75/10a9ebee3fd790d20926a90a2547f0bf78f371b2f13aa822c759680ca7b9/tomli-2.0.1-py3-none-any.whl"
+    sha256 "939de3e7a6161af0c887ef91b7d41a53e7c5a1ca976325f429cb46ea9bc30ecc"
   end
 
   def install
-    # Create a virtual environment
-    venv = libexec
-    system Formula["python@3"].opt_bin/"python3", "-m", "venv", venv
-    
-    # Get paths to Python and pip in the virtual environment
-    venv_python = "#{venv}/bin/python"
-    venv_pip = "#{venv}/bin/pip"
-    
-    # Make sure pip, setuptools, and wheel are up to date in the virtual environment
-    system venv_python, "-m", "pip", "install", "--upgrade", "pip", "setuptools", "wheel"
-    
-    # Install resources
-    resources.each do |r|
-      r.stage do
-        system venv_pip, "install", "--no-deps", "--ignore-installed", "."
-      end
-    end
-    
-    # Install the package without using --prefix and --target together
-    system venv_pip, "install", "--no-deps", "--ignore-installed", "."
-    
-    # Create bin stubs
-    bin.install_symlink Dir["#{venv}/bin/adbstatus*"]
+    virtualenv_install_with_resources
     
     # Create configuration directories
     (etc/"adbstatus/ssl").mkpath
